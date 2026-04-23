bfde69209d9c54a58e1d883a0cef1caf2390623b
rbagin <ravi.bagin@gmail.com>
2026-04-23 22:05:22 +0200

    sync

--- cpp_05/ex01/Form.cpp
+++ cpp_05/ex01/Form.cpp
@@ -6,7 +6,35 @@
 /*   By: rbagin <rbagin@student.codam.nl>             +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2026/04/23 17:24:23 by rbagin        #+#    #+#                 */
-/*   Updated: 2026/04/23 17:24:24 by rbagin        ########   odam.nl         */
+/*   Updated: 2026/04/23 21:27:45 by rbagin        ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include "Form.hpp"
+
+Form::Form()
+{
+	std::cout << "Form Default constructor called" << std::endl;
+}
+
+Form::Form(const Form& other)
+{
+	std::cout << "Form Copy constructor called" << std::endl;
+	*this = other;
+}
+
+Form& Form::operator=(const Form& rhs)
+{
+	std::cout << "Form Copy assignment operator called" << std::endl;
+	if (this != &rhs)
+	{
+		
+	}
+	return (*this);
+}
+
+Form::~Form()
+{
+	std::cout << "Form Destructor called" << std::endl;
+}
 
--- cpp_05/ex01/Form.hpp
+++ cpp_05/ex01/Form.hpp
@@ -6,7 +6,48 @@
 /*   By: rbagin <rbagin@student.codam.nl>             +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2026/04/23 17:24:26 by rbagin        #+#    #+#                 */
-/*   Updated: 2026/04/23 17:24:27 by rbagin        ########   odam.nl         */
+/*   Updated: 2026/04/23 21:37:03 by rbagin        ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
+
+#ifndef FORM_HPP
+# define FORM_HPP
+
+# include <iostream>
+# include <exception>
+# include "Bureaucrat.hpp"
+
+class Form {
+private:
+	const std::string	name;
+	bool				signed;
+	const int			GradeS;
+	const int			GradeEx;
+
+
+public:
+	class GradeTooHighException : public std::exception
+	{
+		public:
+			const char* what() const throw() { return "Grade too high(max 1)!";}
+
+	};
+	class GradeTooLowException : public std::exception
+	{
+		public:
+			const char* what() const throw() { return "Grade too low(min 150)!";}
+	};
+	Form();
+	Form(const Form& other);
+	Form& operator=(const Form& rhs);
+	~Form();
+	const std::string&	getName() const;
+	int					getGradeS() const;
+	int					getGradeEx() const;
+	void				beSigned(const Bureaucrat& b);
+};
+
+std::ostream& operator<<(std::ostream& out, const Form& f);
+
+#endif
 
